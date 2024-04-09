7635f800504649631a08847d9c08e44a055cbb15
DESKTOP-42T4MMM\victo <victorfogaca.medeiros@hotmail.com>
2024-04-08 21:23:29 -0300

    feat: initial commit

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+import Welcome from '../pages/Welcome'
+import Signin from '../pages/Signin'
+import Intro from '../pages/Intro'
+import Cadastrar from '../pages/Cadastrar'
+import EsqueciSenha from '../pages/EsqueciSenha'
+
+const Stack = createNativeStackNavigator();
+
+export default function Routes (){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name="Intro" component={Intro} options={{headerShown: false}}/>
+            <Stack.Screen name="Welcome" component={Welcome} options={{headerShown: false}}/>
+            <Stack.Screen name="Signin" component={Signin} options={{headerShown: false}}/>
+            <Stack.Screen name="Cadastrar" component={Cadastrar} options={{headerShown: false}}/>
+            <Stack.Screen name="EsqueciSenha" component={EsqueciSenha} options={{headerShown: false}}/>
+        </Stack.Navigator>
+    )
+}
